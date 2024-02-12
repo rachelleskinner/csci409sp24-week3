8315c989c20d8f8aa1fdb382035b3a76a8ab9a42
rachelleskinner <rskinner@coastal.edu>
2024-02-12 15:40:26 -0500

    Made Database Modifications

--- api/core/models.py
+++ api/core/models.py
@@ -30,7 +30,7 @@ class Runway(models.Model):
     runway_designation = models.CharField(max_length=1, choices=RUNWAY_DESIGNATION_CHOICES)
     length = models.IntegerField()
     width = models.IntegerField()
-    airport = models.ForeignKey(Airport, on_delete=models.CASCADE)
+    airport = models.ForeignKey(Airport, on_delete=models.CASCADE, related_name="runway_airport")
 
     def __str__(self):
         return f"{self.runway_number}{self.runway_designation}"
@@ -38,7 +38,7 @@ class Runway(models.Model):
 class Flight(models.Model):
     origin = models.ForeignKey(Airport, on_delete=models.PROTECT, related_name="flight_origin")
     destination = models.ForeignKey(Airport, on_delete=models.PROTECT, related_name="flight_destination")
-    airline = models.ForeignKey(Airline, on_delete=models.PROTECT)
+    airline = models.ForeignKey(Airline, on_delete=models.PROTECT, related_name="flight_airline")
     flight_number = models.IntegerField()
     departure = models.DateTimeField()
     arrival = models.DateTimeField()
